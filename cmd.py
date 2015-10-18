c9f095276ac8ea071c934e5f7239ebf7b99e2fad
Priyank Jain <priyankjjain61@gmail.com>
2015-10-18 23:49:27 +0530

    Completed 13 exercised

---
+++ cmd.py
@@ -0,0 +1,3 @@
+import sys
+print sys.argv[0]
+#print "%s" % {sys.argv}
---
+++ ex5.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+my_name = 'Priyank Jain'
+my_age = 22
+my_height = 70
+my_weight = 180
+my_eyes = "black"
+my_hair = "black"
+my_teeth = "white"
+
+print "Let's talk about %s." % {my_name}
+print "He's %d inches tall." % (my_height)
+print "He's %d pounds heavy." % (my_weight)
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair" % (my_eyes,my_hair)
+print "His teeth are usually %s depending on the coffee" % (my_teeth)
+print "If I add %d, %d and %d I get %d" % (my_age,my_height,my_weight,my_height + my_weight + my_age)
